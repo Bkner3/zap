943fc0cb0fc1000c7ec681ec0ae801fa100d13af
Bernardo Quaresma <bernardoquaresma192@gmail.com>
2026-07-21 19:26:41 +0100

    INICIAL commit to repo-tools branch

--- src/utils/move_files.py
+++
@@ -1,8 +0,0 @@
-from os import path, makedirs
-from shutil import move
-
-def move_files(origin, destination):
-    if not path.exists(destination):
-        makedirs(destination)
-    
-    move(origin, path.join(destination, path.basename(origin)))
--- zap.bat
+++
@@ -1,6 +0,0 @@
-@rem This is a batch file to run zap.py with the correct Python interpreter.
-@rem It assumes that Python is installed and added to the system PATH.
-
-@python "%~dp0zap.py" %*
-
-
